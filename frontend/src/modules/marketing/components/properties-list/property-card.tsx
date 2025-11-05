b7c97e9d4763bfe92f8e96dcf44dbf4e5153a8e1
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2025-11-05 14:14:54 -0400

    refactor: files inside modules/marketing/components/properties-list

--- frontend/src/modules/marketing/components/properties-list/property-card.tsx
+++ frontend/src/modules/marketing/components/properties-list/property-card.tsx
@@ -2,24 +2,28 @@ import Image from "next/image";
 
 import { MapPin } from "lucide-react";
 
-import { domicileProp } from "./cardDomicileData";
-export const CardDomicileProp = () => {
+import type { DomicileProp } from "./data";
+
+export const PropertyCard = ({ property }: { property: DomicileProp }) => {
 	return (
 		<article className="bg-white rounded-sm p-3 space-y-2">
 			<Image
 				className="max-w-full object-fit rounded-sm"
-				src={domicileProp.thumbnail}
+				src={property.thumbnail}
 				alt="A simple american house"
 				width={300}
 				height={200}
 			/>
-			<h1 className="font-bold text-lg">{domicileProp.titleProperty}</h1>
+			<h1 className="font-bold text-lg">{property.titleProperty}</h1>
 			<p className="text-lg text-green-700 font-bold rounded-full">
-				{domicileProp.price}
+				{property.price.toLocaleString("en-US", {
+					style: "currency",
+					currency: "USD",
+				})}
 			</p>
 			<div className="flex items-center">
 				<MapPin className="text-gray-600 mr-1" size={15} />
-				<p className="text-gray-600 text-sm">{domicileProp.address}</p>
+				<p className="text-gray-600 text-sm">{property.address}</p>
 			</div>
 			<div className="w-full h-0.5 bg-gray-100 my-3"></div>
 		</article>
