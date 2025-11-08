3277fd9d8e2da4ef3a1e71e0f1b89801853123a2
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2025-11-08 11:22:16 -0400

    feat: added h2 in edit

--- frontend/src/app/(admin)/admin/clients/edit/page.tsx
+++ frontend/src/app/(admin)/admin/clients/edit/page.tsx
@@ -2,6 +2,7 @@ export default function EditClientButton() {
   return (
     <div>
       <h1>Edit Client</h1>
+      <h2 className="text-4xl">This is gonna be a subtitle</h2>
       {/* Add your edit client form here */}
     </div>
   )
