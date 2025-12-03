cb77158a6c36eb0e7d172648e3922e1b54801bd0
Joel <joelangelocano@gmail.com>
2025-12-03 03:12:26 -0500

    feat(frontend): improve form components

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cn } from "@src/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive-foreground text-wrap",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 text-wrap focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive-foreground aria-invalid:bg-destructive",
 	{
 		variants: {
 			variant: {
@@ -13,7 +13,7 @@ const buttonVariants = cva(
 				combobox:
 					"bg-transparent text-input-placeholder border-input-border border text-sm!	px-3!",
 				destructive:
-					"bg-destructive-foreground text-destructive text-whitefocus-visible:ring-destructive-foreground/20 ",
+					"bg-destructive-foreground text-destructive focus-visible:ring-destructive-foreground/20 ",
 				outline:
 					"border bg-transparent text-secondary shadow-xs border-secondary hover:bg-outline-hover hover:text-secondary",
 				secondary:
